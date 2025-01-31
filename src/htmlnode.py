be8c2c895cc2f68768c7b634809adf08acfe69fb
McGruning <markus.gruning@outlook.com>
2025-02-01 10:41:53 +1100

    E: Leaf nodes and tests, begun ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,26 @@ class HTMLNode():
             # List the tag for each child node, or 'None' if the child has no tag
             children_repr = f"[{', '.join(child.tag or 'None' for child in self.children)}]"
         return f"HTMLNode({self.tag}, {self.value}, {children_repr}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value,  props=None):
+        super().__init__(tag, value, props=props)
+        if  self.children is not None:
+            raise AttributeError("LeafNode cannot have children")
+        self.children = None
+
+    @property    
+    def children(self):
+        return None
+    
+    @children.setter
+    def children(self, _):
+        raise AttributeError("LeafNode cannot have children")
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None or self.tag == "":
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
